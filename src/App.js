81ce216be1bee5397697f5f12ee438909c63558e
Rebeca Gaia <reb.gaia@gmail.com>
2021-07-14 19:21:40 -0300

    hook state using function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0); // primeiro valor igual à zero
+
+  const handleClick = () => {
+    setCount(pState => pState + 1); // pState pega o estado anterior
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Você clicou {count} vezes</p>
+      <button onClick={handleClick}>
+        Clique aqui
+      </button>
     </div>
   );
 }
